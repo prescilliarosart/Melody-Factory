2e1355894b6a65572be85ad96bdad0a7cf3a86d7
Prescillia Rosart <prescillia.rosart@gmail.com>
2025-12-18 16:36:12 +0100

    mediator

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 let score = 0;
 let clickValue = 1;
-const scoreDisplay = document.getElementById("score");
+let mediatorLevel = 0;
+let mediatorPrice = 10;
 
+const scoreDisplay = document.getElementById("score");
 const guitarClick = document.querySelector(".onclick");
+const mediator = document.querySelector(".mediator");
 
 function updateDisplay() {
     scoreDisplay.textContent = Math.floor(score);
 }
 
-guitarClick.addEventListener("click",() => {
+guitarClick.addEventListener("click", () => {
     score += clickValue;
     updateDisplay();
-   
+
 });
+
+
+mediator.addEventListener("click", () => {
+    if (score < mediatorPrice) {
+        return
+    }
+
+    score -= mediatorPrice;
+    mediatorLevel += 1;
+    clickValue += 1;
+    mediatorPrice = mediatorPrice * 1.2;
+    updateDisplay();
+
+});
+
+
